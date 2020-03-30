c1d4b723aa1bf5b6046d5a559f9802971fd09186
blackinferno1000 <seanalexander360@gmail.com>
2020-03-30 04:20:29 -0400

    part c complete

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
+const redis = require('redis');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -24,26 +28,56 @@ mongoose.connect(dbURL, mongooseOptions, (err) => {
   }
 });
 
+let redisURL = {
+    hostname: 'redis-15618.c52.us-east-1-4.ec2.cloud.redislabs.com',
+    port: 15618,
+};
+
+let redisPASS = 'nSzX8OfnfcYuNPrkXVyrRIJ3piqkuiHO';
+if(process.env.REDISCLOUD_URL) {
+    redisURL = url.parse(process.env.REDISCLOUD_URL);
+    redisPASS = redisURL.auth.split(':')[1];
+}
+let redisClient = redis.createClient({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    password: redisPASS,
+});
+
 const router = require('./router');
 
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 app.use(session({
-    key: 'sessionid',
-    secret: 'Domo Arigato',
-    resave: true,
-    saveUninitialized: true,
+  key: 'sessionid',
+  store: new RedisStore({
+      client: redisClient,
+  }),
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+      httpOnly: true,
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if(err.code !== 'EBADCSRFTOKEN') { return next(err); }
+
+    console.log('Missing CSRF token');
+    return false;
+});
 
 router(app);
 
